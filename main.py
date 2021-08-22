4e025fd3fe551ce20482679cd93657b9e2dc2c75
1156496364 <1527099663@qq.com>
2021-08-22 23:05:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # 定义字典处理函数，接受通过readlines方法读入的对象，输出去掉换行的列表
-from os import write
+import os
 
+ 
 
 def list_solve(f):
     ls=[]
@@ -17,26 +18,27 @@ def write_dict(a):
     with open('1.txt','a') as fp:
         fp.write(a+"\n")
 
+def program_main():
+    # 定义ci变量用来计数新增了多少
+    ci=0       
 
-# 定义ci变量用来计数新增了多少
-ci=0       
+    # 读取主字典，生成密码列表，方便查重
+    with open('1.txt','r') as fp:
+        f=fp.readlines()
+        ls=list_solve(f)
+        
 
-# 读取主字典，生成密码列表，方便查重
-with open('1.txt','r') as fp:
-    f=fp.readlines()
-    ls=list_solve(f)
-    
+    # 读取新字典，生成密码列表
+    with open('2.txt','r') as fp2:
+        g=fp2.readlines()
+        ls2=list_solve(g)
+        
+    # 进行比较，主字典没有的写入
+    for items in ls2:
+        if items not in ls:
+            write_dict(items)
+            ci+=1
+        else:
+            pass
+    print("新增"+str(ci)+"项")
 
-# 读取新字典，生成密码列表
-with open('3.txt','r') as fp2:
-    g=fp2.readlines()
-    ls2=list_solve(g)
-    
-# 进行比较，主字典没有的写入
-for items in ls2:
-    if items not in ls:
-        write_dict(items)
-        ci+=1
-    else:
-        pass
-print("新增"+str(ci)+"项")
